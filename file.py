cec66c378adb90daa8112c115e4840ef06fcdf3f
SimonAllenOliver <simonallenoliver@gmail.com>
2024-01-08 17:16:11 -0600

    stuff

--- file.py
+++ file.py
@@ -8,66 +8,66 @@ fruit = ('blueberry', 'strawberry', 'banana') #tuple, initialize; var declaratio
 print(type(fruit)) #type check 
 print(pizza_toppings[1]) #list, access value
 pizza_toppings.append('Mushrooms') #list, add value
-print(person['name']) 
-person['name'] = 'George'
-person['eye_color'] = 'blue'
-print(fruit[2])
+print(person['name']) #dictionary, access value
+person['name'] = 'George' #dictionary, change value
+person['eye_color'] = 'blue' #dictionary, add value
+print(fruit[2]) #tuple, access value
 
-if num1 > 45:
-    print("It's greater")
+if num1 > 45: 
+    print("It's greater") 
 else:
-    print("It's lower")
+    print("It's lower") #conditional if else statement
 
 if len(string) < 5:
     print("It's a short word!")
 elif len(string) > 15:
     print("It's a long word!")
 else:
-    print("Just right!")
+    print("Just right!") #else if statement
 
 for x in range(5):
-    print(x)
+    print(x) #for loop from 0 to 5 with increment 1
 for x in range(2,5):
-    print(x)
-for x in range(2,10,3):
-    print(x)
+    print(x) #for loop from 2 to 5 increment 1
+for x in range(2,10,3): 
+    print(x) #for loop from 2 to 10 increment 3
 x = 0
 while(x < 5):
     print(x)
-    x += 1
+    x += 1  #while loop start 0, end 5, increment 1
 
-pizza_toppings.pop()
-pizza_toppings.pop(1)
+pizza_toppings.pop() #list remove from end
+pizza_toppings.pop(1) #list remove from postion 1
 
 print(person)
 person.pop('eye_color')
-print(person)
+print(person) #prints dict, removed var from dict, prints dict
 
 for topping in pizza_toppings:
     if topping == 'Pepperoni':
         continue
     print('After 1st if statement')
     if topping == 'Olives':
-        break
+        break #for loop 2 if statements re a list
 
 def print_hello_ten_times():
     for num in range(10):
-        print('Hello')
+        print('Hello') #function defined, for loop 0 to 10 incr 1
 
-print_hello_ten_times()
+print_hello_ten_times() #calls above function
 
 def print_hello_x_times(x):
     for num in range(x):
-        print('Hello')
+        print('Hello') #creates funciton with parameter for for loop range
 
-print_hello_x_times(4)
+print_hello_x_times(4) #calls above function with for loop range 0 to 4 inc 1
 
 def print_hello_x_or_ten_times(x = 10):
     for num in range(x):
-        print('Hello')
+        print('Hello') #same as above except range starts 0 ends 10 inc 1
 
-print_hello_x_or_ten_times()
-print_hello_x_or_ten_times(4)
+print_hello_x_or_ten_times() 
+print_hello_x_or_ten_times(4) #calls function with default range of 10, and calls again with new range of 4
 
 
 """
